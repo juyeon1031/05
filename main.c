d188a71787cac910a205766f7d33555b9a996171
juyeon1031 <113417188+juyeon1031@users.noreply.github.com>
2022-10-04 17:13:52 +0900

    03

--- main.c
+++ main.c
@@ -4,13 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
-	printf("정수 하나를 입력하세요:");
-	scanf("%i", &i);
-
-	if(i<0)
-		i = i*(-1);
+	char c;
+	int num=0;
 	
-	printf("절댓값은 %i 입니다\n", i);
+	printf("input a string: ");
+	
+	while ((c = getchar())!= '\n')
+	{
+		if(c>='0' && c<='9')
+			num++;
+    }
+	printf("the number of digits is %i.\n", num);
 	return 0;
 }
